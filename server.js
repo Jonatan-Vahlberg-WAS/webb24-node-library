e946f4a2f72560a75c1b43c2a7aabd1f28380920
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-02-07 11:51:13 +0100

    add book requests

--- server.js
+++ server.js
@@ -39,10 +39,12 @@ app.get('/api/hello-world/', (req, res) => {
 // kan också filtreras med ?q
 app.get('/api/authors/', async (req, res) => {
   let query = 'SELECT * FROM Authors'
+  let params = []
   if(req.query.q) {
-    query = `${query} WHERE name ILIKE '%${req.query.q}%'`
+    query = `${query} WHERE name ILIKE $1`
+    params.push(`%${req.query.q}%`)
   }
-  const result = await pool.query(query)
+  const result = await pool.query(query, params)
   const authors = result.rows
   res.json(authors)
 })
@@ -67,6 +69,41 @@ app.post('/api/authors/', async (req, res) => {
   )
   const newAuthor = result.rows?.[0]
   res.status(201).json(newAuthor)
+})
+
+app.get('/api/books/', async (req,res) => {
+  const result = await pool.query(
+    'SELECT books.id, title, stock, Authors.id as author_id, Authors.name as author_name FROM BOOKS INNER JOIN Authors ON books.author_id = Authors.id'
+  )
+  const books = result.rows.map(book => {
+    return {
+      id: book.id,
+      title: book.title,
+      stock: book.stock,
+      author: {
+        id: book.author_id,
+        name: book.author_name
+      }
+    }
+  })
+  return res.json(books)
+})
+
+//POST anrop som skapar en book
+app.post('/api/books/', async (req,res) => {
+  const {title, stock, author_id} = req.body
+  if(!title || !stock || !author_id){
+    return res.status(400).json({
+      message: "ERROR: title, stock and author_id is required"
+    })
+  }
+  const result = await pool.query(
+    'INSERT INTO BOOKS (title, author_id, stock) VALUES($1,$2,$3) RETURNING *',
+    [title, author_id, stock]
+  )
+  const book = result.rows?.[0]
+  //TODO: handle dabase errors
+  return res.status(201).json(book)
 })
 
 
