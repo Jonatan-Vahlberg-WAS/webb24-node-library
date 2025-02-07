e29f6ea244a18b5b3022cdf788f80021e835cf11
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-02-07 13:49:18 +0100

    add sequelize

--- models.js
+++ models.js
@@ -6,6 +6,10 @@ const Author = sequelize.define('author',
         name: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        yearOfBirth: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0
         }
     },
     {
--- ormRouter.js
+++ ormRouter.js
@@ -1,10 +1,20 @@
 const { Author } = require("./models")
+const { Op } = require('sequelize');
 const {Router} = require("express")
 
 const router = Router()
 
 router.get("/authors/", async (req,res) => {
-  const authors = await Author.findAll()
+  const q = req.query.q
+  const options = {}
+  if(q) {
+    options.where = {
+      name: {
+        [Op.iLike]: `%${q}%`
+      }
+    }
+  }
+  const authors = await Author.findAll(options)
 
   return res.json(authors)
 })
